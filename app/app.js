9cf3b69766c1a5bababe020a1cd4e4d98008efdc
LuisFranciscoCode <luisfranciscocode@gmail.com>
2017-04-17 12:27:15 +0100

    save

--- app/app.js
+++ app/app.js
@@ -1,8 +1,5 @@
 var application = require("application");
 
-<<<<<<< HEAD
 application.start({ moduleName: "views/main-api-view/main-api" });
-=======
-application.start("Start-Page/Start-Page");
->>>>>>> refs/remotes/origin/Start-Page
+//application.start("Start-Page/Start-Page");
 
--- app/views/main-api-view/main-api.js
+++ app/views/main-api-view/main-api.js
@@ -1,6 +1,7 @@
 var buttonModule = require("ui/button");
 var labelModule = require("ui/label");
 var stackLayout = require("ui/layouts/stack-layout");
+var gridLayout = require("ui/layouts/grid-layout");
 
 var formattedStringModule = require("text/formatted-string");
 var spansModule = require("text/span");
@@ -22,31 +23,56 @@ dataPropertys = function() {
 }
 
 drawForm = function(txt, tp, typeObj, icon) {
-    var layout = new stackLayout.StackLayout();
+    // var layout = new stackLayout.StackLayout();
+    var layout = new gridLayout.GridLayout();
+
     var button = new Array();
     var label = new Array();
     var num = typeObj.length;
-    
+
+    var x = 0;
+    var y = 0;
+
     for (i = 0; i <= num; i++) {
-        const cont = i;       
+        const cont = i;  
 
         switch (typeObj[i]) {
             case "Label":
                 label[cont] = new labelModule.Label();
                 label[cont].text = txt[cont];
-                layout.addChild(label[cont]);
+                // layout.addChild(label[cont]);
                 break;
 
-            case "Button":
+            case "Button":   
                 var textSpan = new spansModule.Span();
+                var iconSpan = new spansModule.Span();
                 var formattedString = new formattedStringModule.FormattedString();
-                textSpan.text = String.fromCharCode(icon[cont]) + "\n" + txt[cont];
+                iconSpan.text = String.fromCharCode(icon[cont]);
+                iconSpan.fontSize = 25;
+                formattedString.spans.push(iconSpan);
+
+                textSpan.text = "\n\n" + txt[cont];                
                 formattedString.spans.push(textSpan);
 
                 button[cont] = new buttonModule.Button();
-                button[cont].className = "btnIcon";               
+                button[cont].className = "btnIcon";            
                 button[cont].formattedText = formattedString;
 
+                // draw grid
+                if (x <= 1) {
+                    gridLayout.GridLayout.setColumn(button[cont], x);
+                }
+                else {
+                    gridLayout.GridLayout.setRow(button[cont], 1);
+                    gridLayout.GridLayout.setColumn(button[cont], y);
+                    y += 1;
+                }
+                x += 1;
+
+                var column = new gridLayout.ItemSpec(1, gridLayout.GridUnitType.auto);
+                var row = new gridLayout.ItemSpec(1, gridLayout.GridUnitType.auto);
+                layout.addColumn(column);
+                layout.addRow(row);
                 layout.addChild(button[cont]);
                 break;
         }        
@@ -61,5 +87,5 @@ exports.options0 = function() {
 }
 
 exports.options1 = function() {
-    alert("OPTIONS 1");
+    alert("OPTIONS 1"); 
 }
