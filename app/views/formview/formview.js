9d9ef320efe00a2a5a9b82ee5e83a2b2343dffda
LuisFranciscoCode <luisfranciscocode@gmail.com>
2017-06-05 15:31:17 +0100

    Falta adicionar os dados

--- app/views/formview/formview.js
+++ app/views/formview/formview.js
@@ -19,12 +19,15 @@ exports.formView = function(args) {
     var scrollView = new scrollModule.ScrollView();
     var fieldsArray = [];
 
+    var saveInputText = [];
+    var q = 0;
+    
     for(cont=0; cont < data.length; cont++)
     {
         const i = cont;
+        const p = q;
         switch(data[i].type){
             case "checkbox":
-
                 /*fieldsArray[cont] = new checkModule.CheckBox();
                 fieldsArray[cont].value = data[cont].value;
                 fieldsArray[cont].id = data[cont].id;
@@ -38,21 +41,21 @@ exports.formView = function(args) {
                 fieldsArray[i].items = data[i].items;
                 fieldsArray[i].id = data[i].id;
                 fieldsArray[i].selectedIndex = 0;
-                newStackLayout.addChild(fieldsArray[i]);
-               // testarArray[i] = data[i].type;
+
+                //console.info("coco: " + i);
+                //saveInputText[p] = i;
+                //q += 1;
                 break;
             case "radiogroup":
                 /*fieldsArray[cont] = new radioBtnModule.RadioGroup();
                 fieldsArray[cont].id = data[cont].id;
                 newStackLayout.addChild(fieldsArray[cont]);*/
-        
                 break;
             case "radiobutton":
                 /*fieldsArray[cont] = new radioBtnModule.RadioButton();
                 fieldsArray[cont].id = data[cont].id;
                 fieldsArray[cont].text = data[cont].text;
                 fieldsArray[cont].value = data[cont].value;
-
                 newStackLayout.getViewById(data[cont].group).addChild(fieldsArray[cont]);*/
                 break;
             case "label":
@@ -66,78 +69,37 @@ exports.formView = function(args) {
                 fieldsArray[i].hint = data[i].hint;
                 fieldsArray[i].id = data[i].id;
                 newStackLayout.addChild(fieldsArray[cont]);
+                saveInputText[p] = i;
+                q += 1;
+                console.info("textfield: " + i);
                 break;
             case "textview":
                 fieldsArray[i] = new textViewModule.TextView();
                 fieldsArray[i].hint = data[i].hint;
                 fieldsArray[i].id = data[i].id;
                 newStackLayout.addChild(fieldsArray[i]);
+                saveInputText[p] = i;
+                q += 1;
+                console.info("textview: " + i);
                 break;
             default:
                 break;
         }
     }
 
+    // button submit
     var submitBtn = new buttonModule.Button();
-    submitBtn.text = "submit";
+    submitBtn.text = "submit";  
+    submitBtn.on(buttonModule.Button.tapEvent, function (){
+
+        for (i = 0; i < saveInputText.length; i++) {
+            var x = saveInputText[i];
+            console.info(fieldsArray[x].text);
+            con.add('/aasd', fieldsArray[x].text);
+        }   
+    });
+
     newStackLayout.addChild(submitBtn);
     scrollView.content = newStackLayout;
     page.content = scrollView;
-
-    // event button
-    submitBtn.on(buttonModule.Button.tapEvent, function (){
-        var submitInfo = new Array();
-        var varNames = new Array();
-
-        for(cont = 0; cont < inputObject.length; cont++)
-        {
-            var i = cont;
-            switch(inputObject)
-            {
-                case "textfield":
-                    testarArray[i] = fieldsArray[i].text;
-                    varNames[i] = data[i].varName;
-                    break;
-                case "dropdown":
-                    testarArray[i] = fieldsArray[i].items[fieldsArray[i].selectedIndex];
-                    varNames[i] = data[i].varName;
-                break;
-               /* case "checkbox":
-                        submitInfo[i] = fieldsArray[i].checked;
-                        varNames[i] = data[i].varName;
-                break;
-                case "radiogroup":
-                        var countt = fieldsArray[i]._childrenCount;
-                        for(j = 0;j < countt; j ++){
-                            if(fieldsArray[i]._subViews[j].checked == true){
-                                submitInfo[i-cont] = fieldsArray[i]._subViews[j].value;
-                                varNames[i-cont] = data[i].varName;
-                            }
-                            else{
-                                submitInfo[i-cont] = "null";
-                                varNames[i-cont] = data[i].varNames;
-                            }
-                        }
-                break; */
-                case "textview":
-                    testarArray[i] = fieldsArray[i].text;
-                    varNames[i] = data[i].varName;
-                    break;
-                case "label":
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        // con.add('/aasd', submitInfo);
-        // alert("Sended to database");
-        // var navigationOptions =
-        // {
-        //     moduleName: "views/main-api-view/main-api",
-        //     clearHistory: true
-        // }
-        // var topmost = frameModule.topmost();
-        // topmost.navigate(navigationOptions);
-    });
 }
