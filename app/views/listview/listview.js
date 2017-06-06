482a0895c9cfe58692b44b00615ce51edc451809
LuisFranciscoCode <luisfranciscocode@gmail.com>
2017-06-06 18:26:05 +0100

    Listview working

--- app/views/listview/listview.js
+++ app/views/listview/listview.js
@@ -14,9 +14,12 @@ exports.listView = function(args) {
 
     var gridLayout = page.getViewById("testar");
     var labelTitle = [];
+
     for(i = 0; i < lista.length; i++) {
         labelTitle[i] = new labelModule.Label();
         labelTitle[i].text = lista[i];
+        labelTitle[i].marginRight = "5";
+        labelTitle[i].color = "Red";
 
         gridModule.GridLayout.setColumn(labelTitle[i], i);
         gridModule.GridLayout.setRow(labelTitle[i], 0);
@@ -31,25 +34,30 @@ exports.listView = function(args) {
 
     var data = localStorage.getItem("aasd");
     var arr = Object.values(data);
+
+    console.info(arr.length);
+    console.info(arr[0].length);
+    console.info(JSON.stringify(arr));
     
-    var merdaQualquer = page.getViewById("bueda");
     var labellist = [];
     for(i = 0; i < arr.length; i++) {
-        for(x = 0; x < arr.length; x++)
+        for(x = 0; x < arr[i].length; x++)
         {
             labellist[i] = new labelModule.Label();
-            labellist[i].text = "cenas";
+            labellist[i].text = arr[i][x];
+            labellist[i].marginRight = "5";
+            console.info(arr[i][x]);
 
-            gridModule.GridLayout.setColumn(labellist[i], i);
-            gridModule.GridLayout.setRow(labellist[i], x);
+            gridModule.GridLayout.setColumn(labellist[i], x);
+            gridModule.GridLayout.setRow(labellist[i], i + 1);
 
             var column = new gridModule.ItemSpec(1, "auto");
             var row = new gridModule.ItemSpec(1, "auto");
 
             gridLayout.addColumn(column);
-            gridLayout.addRow(row);
             gridLayout.addChild(labellist[i]);
         }
+            gridLayout.addRow(row);
     }
 
    // page.content = gridLayout;
