7057b2a4d615638d725c1264b522976283d29d4c
Micael Rodrigues <micaelr95@outlook.pt>
2017-06-12 17:44:56 +0100

    Added bottom navigation to form view

--- app/views/formview/formview.js
+++ app/views/formview/formview.js
@@ -9,110 +9,85 @@ var topmost = require("ui/frame").topmost();
 var Connection = require("../../shared/DB_connection");
 var con = new Connection();
 
+var options = localStorage.getItem("Options");
 var data = localStorage.getItem("constructForm");
 
 exports.formView = function(args) {
     var page = args.object;
-
-    var options = localStorage.getItem("Options");
     page.actionBar.title = "FormView";
     page.actionBar.backgroundColor = options.color_actionBar;
     page.actionBar.color = options.color_text;
     
-    var newStackLayout = new stackModule.StackLayout();
-    var scrollView = new scrollModule.ScrollView();
-
-    var fieldsArray = [];       // save objects
-    var submitInfo = [];        // save info to submit
-    var saveInputText = [];     // save number of objects with input
-    var q = 0;
-    
-    for(cont=0; cont < data.length; cont++)
+    var container = page.getViewById("Container");
+    if (container._childrenCount == 0)
     {
-        const i = cont;
-        const p = q;
-        switch(data[i].type){
-            case "checkbox":
-                /*fieldsArray[cont] = new checkModule.CheckBox();
-                fieldsArray[cont].value = data[cont].value;
-                fieldsArray[cont].id = data[cont].id;
-                fieldsArray[cont].text = data[cont].text;
-
-                newStackLayout.addChild(fieldsArray[cont]);*/
-                break;
-            case "dropdown":
-                var arrayDados = new Array();
-                fieldsArray[i] = new dropModule.DropDown();
-                fieldsArray[i].items = data[i].items;
-                fieldsArray[i].id = data[i].id;
-                fieldsArray[i].selectedIndex = 0;
-                newStackLayout.addChild(fieldsArray[cont]);
-                saveInputText[p] = i;
-                q += 1;
-                console.info("drop: " + i);
-                break;
-            case "radiogroup":
-                /*fieldsArray[cont] = new radioBtnModule.RadioGroup();
-                fieldsArray[cont].id = data[cont].id;
-                newStackLayout.addChild(fieldsArray[cont]);*/
-                break;
-            case "radiobutton":
-                /*fieldsArray[cont] = new radioBtnModule.RadioButton();
-                fieldsArray[cont].id = data[cont].id;
-                fieldsArray[cont].text = data[cont].text;
-                fieldsArray[cont].value = data[cont].value;
-                newStackLayout.getViewById(data[cont].group).addChild(fieldsArray[cont]);*/
-                break;
-            case "label":
-                fieldsArray[i] = new labelModule.Label();
-                fieldsArray[i].id = data[i].id;
-                fieldsArray[i].text = data[i].text;
-                newStackLayout.addChild(fieldsArray[cont]);
-                break;
-            case "textfield":
-                fieldsArray[i] = new textFieldModule.TextField();
-                fieldsArray[i].hint = data[i].hint;
-                fieldsArray[i].id = data[i].id;
-                newStackLayout.addChild(fieldsArray[cont]);
-                saveInputText[p] = i;
-                q += 1;
-                console.info("textfield: " + i);
-                break;
-            case "textview":
-                fieldsArray[i] = new textViewModule.TextView();
-                fieldsArray[i].hint = data[i].hint;
-                fieldsArray[i].id = data[i].id;
-                newStackLayout.addChild(fieldsArray[i]);
-                saveInputText[p] = i;
-                q += 1;
-                console.info("textview: " + i);
-                break;
-            default:
-                break;
+        var fieldsArray = [];       // save objects
+        var submitInfo = [];        // save info to submit
+        var saveInputText = [];     // save number of objects with input
+        var q = 0;
+        
+        for(cont=0; cont < data.length; cont++)
+        {
+            const i = cont;
+            const p = q;
+            switch(data[i].type){
+                case "dropdown":
+                    var arrayDados = new Array();
+                    fieldsArray[i] = new dropModule.DropDown();
+                    fieldsArray[i].items = data[i].items;
+                    fieldsArray[i].id = data[i].id;
+                    fieldsArray[i].selectedIndex = 0;
+                    container.addChild(fieldsArray[cont]);
+                    saveInputText[p] = i;
+                    q += 1;
+                    console.info("drop: " + i);
+                    break;
+                case "label":
+                    fieldsArray[i] = new labelModule.Label();
+                    fieldsArray[i].id = data[i].id;
+                    fieldsArray[i].text = data[i].text;
+                    container.addChild(fieldsArray[cont]);
+                    break;
+                case "textfield":
+                    fieldsArray[i] = new textFieldModule.TextField();
+                    fieldsArray[i].hint = data[i].hint;
+                    fieldsArray[i].id = data[i].id;
+                    container.addChild(fieldsArray[cont]);
+                    saveInputText[p] = i;
+                    q += 1;
+                    console.info("textfield: " + i);
+                    break;
+                case "textview":
+                    fieldsArray[i] = new textViewModule.TextView();
+                    fieldsArray[i].hint = data[i].hint;
+                    fieldsArray[i].id = data[i].id;
+                    container.addChild(fieldsArray[i]);
+                    saveInputText[p] = i;
+                    q += 1;
+                    console.info("textview: " + i);
+                    break;
+            }
         }
+    
+        // button submit
+        var submitBtn = new buttonModule.Button();
+        submitBtn.text = "submit";  
+        submitBtn.on(buttonModule.Button.tapEvent, function (){
+
+            for (i = 0; i < saveInputText.length; i++) {
+                var x = saveInputText[i];
+
+                if (data[x].type == 'dropdown') {
+                    submitInfo[i] = fieldsArray[x].items[fieldsArray[x].selectedIndex];
+                }
+                else {
+                    submitInfo[i] = fieldsArray[x].text;
+                }
+            }
+            con.add('/aasd', submitInfo);
+        });
+        container.addChild(submitBtn);
     }
-
-    // button submit
-    var submitBtn = new buttonModule.Button();
-    submitBtn.text = "submit";  
-    submitBtn.on(buttonModule.Button.tapEvent, function (){
-
-        for (i = 0; i < saveInputText.length; i++) {
-            var x = saveInputText[i];
-
-            if (data[x].type == 'dropdown') {
-                submitInfo[i] = fieldsArray[x].items[fieldsArray[x].selectedIndex];
-            }
-            else {
-                submitInfo[i] = fieldsArray[x].text;
-            }
-        }
-        con.add('/aasd', submitInfo);
-    });
-
-    newStackLayout.addChild(submitBtn);
-    scrollView.content = newStackLayout;
-    page.content = scrollView;
 }
 
 exports.homeButton = function()
