2359b353ad957568a59686d2b1830f3695561b7e
Micael Rodrigues <micaelr95@outlook.pt>
2017-06-21 17:10:04 +0100

    Server now show listening ip

--- Server/Server.js
+++ Server/Server.js
@@ -1,6 +1,31 @@
-var http = require('http'),
-    fileSystem = require('fs'),
-    path = require('path');
+var http = require('http');
+var fileSystem = require('fs');
+var path = require('path');
+var os = require('os');
+
+var ifaces = os.networkInterfaces();
+var localIp;
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      //console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      //console.log(ifname, iface.address);
+      localIp = iface.address;
+    }
+    ++alias;
+  });
+});
 
 console.log("Initiating");
 http.createServer(function(request, response) {
@@ -16,4 +41,4 @@ http.createServer(function(request, response) {
     var readStream = fileSystem.createReadStream(filePath);
     readStream.pipe(response);
 }).listen(2000);
-console.log('Server running at http://localhost:2000/');
+console.log('Server listening at http://'+ localIp + ':2000/');
